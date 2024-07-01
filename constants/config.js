e3b5d581c8ddaaab2be899e2e07e11e88d002f50
Jashhmehta <jashmehtaa@gmail.com>
2024-07-01 23:04:42 +0530

    Updated config.js

--- constants/config.js
+++ constants/config.js
@@ -4,9 +4,7 @@ dotenv.config({
   });
 export const corsOptions = {
     origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "https://pingme-frontend-z7gx.vercel.app",
+      process.env.CLIENT_URL,
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
